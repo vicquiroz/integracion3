076601caef43b8deff6b3a0495318759ab52fd13
Eduardo Lobos <elobos2018@alu.uct.cl>
2020-10-26 16:48:04 -0300

    Se logra graficar los datos y almacenar el grafico en imagen
    De una manera simple se logra graficar los datos obtenidos del JSON, se cambio nombre de funciones para mayor comprensión y a su vez se agregaron comentarios en el código

--- PythonDC/Prueba.py
+++ PythonDC/Prueba.py
@@ -1,53 +1,60 @@
 #!/usr/bin/env python
 # -- coding: utf-8 --
 import json
+from matplotlib import pyplot as plt
 
-def calc(dato,petUno,petDos):
-    guar = []
+def mostrar(dato,petUno,petDos):
+    guar = [] #arreglo que contendra los datos pedidos
     for i in range(len(dato)):
-        pos = dato[i]
-        dat = pos[petUno]
-        if (isinstance(dat, list)):
-            for j in range(len(dat)):
-                bus = dat[j]
-                guar.append(bus[petDos])
+        pos = dato[i] #almacena los datos
+        dat = pos[petUno] #va a la direccion de la primera peticion
+        if (isinstance(dat, list)): #si la primera peticion es un arreglo
+            for j in range(len(dat)): 
+                bus = dat[j] #almacena los datos
+                guar.append(bus[petDos])#guarda los datos de la segunda peticion 
                 
-        else:
-            guar.append(dat)
+        else: #sí el anterior no es un arreglo 
+            guar.append(dat) #se almacenan los datos 
     return(guar)        
 
 def suma(val):
-    nombres = []
-    cont = []
-    nombres.append(val[0])
-    cont.append(0)
+    nombres = [] #arreglo que contiene los nombres
+    cont = [] #arreglo que contiene la cantidas de veces repetido el nombre
+    nombres.append(val[0])#se necesita que se inicie con un valor
+    cont.append(0)#se necesita que se inicie con un valor
     for i in range(len(val)):
-        resNom = nombres
-        resCont = cont
+        resNom = nombres #comparador de nombres
+        resCont = cont #comparador de cantidad
         
         for j in range(len(nombres)):
     
-            if nombres[j] == val[i]:
-                cont[j] = cont[j]+1
-                resCont = None
-                resNom = None
+            if nombres[j] == val[i]: #si detecta que almacenado en nombre existe una variable igual a la de los datos
+                cont[j] = cont[j]+1 #le suma 1 a su contador correspondiente
+                resCont = None #se reinicia el comparador
+                resNom = None #se reinicia el comparador
 
-        if resNom == nombres and resCont == cont:
-            nombres.append(val[i])
-            cont.append(1)
+        if resNom == nombres and resCont == cont: #si las variables y sus comparadores coinciden
+            nombres.append(val[i]) #se agrega el nombre
+            cont.append(1) #se agrega un 1 al contador 
 
-    print(nombres)
-    print(cont)
+    return nombres, cont
         
-        
+    
+def graficar(ejex, ejey):
+    ancho= 0.5 #ancho para el grafico de barras
+    plt.bar(ejex, ejey, ancho) #se pasan los datos para completar 
+    plt.savefig('grafico.png', transparent=False)#se exporta el grafico en png
+    plt.show()
+
+with open('His_clin.json', encoding='utf-8') as file: #abrir imagen con utf-8 para mayor comprension
+    data = json.load(file) #guardamos el contenido del json en data
 
 
-with open('His_clin.json', encoding='utf-8') as file:
-    data = json.load(file)
 
-    res = calc(data,'sesiones_medica','nombre_sesion')
-    print(res)
-    suma(res)
+####llamada a los def#####
+    res = mostrar(data,'sesiones_medica','nombre_profesional')
+    nom,val = suma(res)
+    graficar(nom, val)
 
 
     
