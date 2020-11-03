a0b7db90ef6081eacf73460c1400e6e5eb7fc54a
Victor Quiroz <victor.antonio.quiroz.2017@gmail.com>
2020-11-03 20:50:35 -0300

    implementación de dataframe mediante pandas
    se crea un dataframe, el cual en un futuro nos servirá para hacer trabajos estadísticos con las edades de los pacientes, las cuales también se están calculando a partir de su fecha de nacimiento.
    By: Eduardo Lobos y Victor Quiroz.

--- PythonDC/Prueba.py
+++ PythonDC/Prueba.py
@@ -2,19 +2,27 @@
 # -- coding: utf-8 --
 import json
 from matplotlib import pyplot as plt
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+def edad(dato):
+    fecha = dato.split("-")
+    fechaStr = fecha[2]+"-"+fecha[1]+"-"+fecha[0]
+    fecha_nacimiento = datetime.strptime(fechaStr, "%d-%m-%Y")
+    edad = relativedelta(datetime.now(), fecha_nacimiento)
+    return edad.years
+
 
 def mostrar(dato,petUno,petDos):
     guar = [] #arreglo que contendra los datos pedidos
     for i in range(len(dato)):
-        pos = dato[i] #almacena los datos
-        dat = pos[petUno] #va a la direccion de la primera peticion
-        if (isinstance(dat, list)): #si la primera peticion es un arreglo
-            for j in range(len(dat)): 
-                bus = dat[j] #almacena los datos
-                guar.append(bus[petDos])#guarda los datos de la segunda peticion 
+        if (isinstance(dato[i][petUno], list)): #si la primera peticion es un arreglo
+            for j in range(len(dato[i][petUno])): 
+                guar.append(dato[i][petUno][j][petDos]) #guarda los datos de la segunda peticion 
                 
         else: #sí el anterior no es un arreglo 
-            guar.append(dat) #se almacenan los datos 
+            guar.append(dato[i][petUno]) #se almacenan los datos 
     return(guar)        
 
 def suma(val):
@@ -46,15 +54,33 @@ def graficar(ejex, ejey):
     plt.savefig('grafico.png', transparent=False)#se exporta el grafico en png
     plt.show()
 
-with open('His_clin.json', encoding='utf-8') as file: #abrir imagen con utf-8 para mayor comprension
-    data = json.load(file) #guardamos el contenido del json en data
+def tabla(dato):
+    fig, ax = plt.subplots(dpi=500)
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    d = dato[0]
+    del d['profesionales_que_atendieron']
+    del d['sesiones_medica']
+    print(d)
+    df = pd.DataFrame(data=d)
+    print(df)
+    ax.table(cellText=df.values, rowLabels=["valor"],colLabels=df.columns,cellLoc='center', loc='center')
+    fig.tight_layout()
+    plt.show()
 
+with open('His_clin.json', encoding='utf-8') as file: #abrir imagen con utf-8 para mayor comprension
+    dato = json.load(file) #guardamos el contenido del json en data
+    #print(data[0]['sesiones_medica'][0]['nombre_sesion'])
 
 
 ####llamada a los def#####
-    res = mostrar(data,'sesiones_medica','nombre_profesional')
-    nom,val = suma(res)
-    graficar(nom, val)
+    res = mostrar(dato,'sesiones_medica','nombre_profesional')
+    tabla(dato)
+    edad = edad(dato[0]['fecha_nacimiento'])
+    print(edad)
+
+    #nom,val = suma(res)
+    #graficar(nom, val)
 
 
     
