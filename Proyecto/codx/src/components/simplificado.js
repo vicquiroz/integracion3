e7fe82ae11a08e7b4a5193009ee576f2be1a1a1f
Fernando Torres <ftorres2018@alu.uct.cl>
2020-11-13 17:50:14 -0300

    Menu desplegable aplicado
    Se aplico una barra desplegable en la cual van a ir las funciones que calcularan estadígrafos.
    By : Antonio Parra, Fernando Torres

--- Proyecto/codx/src/components/simplificado.js
+++ Proyecto/codx/src/components/simplificado.js
@@ -3,6 +3,9 @@ import { Nav, NavItem, Button,Input } from 'reactstrap';
 import {ConseguirArchivo, PostDatos,Graficar, GraficarDesdeArchivo} from './consultas';
 import './comps.css';
 
+import {Menu,MenuItem } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+
 export const Sim = (props) => {
   return (
     <div id="Simplificado">
@@ -19,11 +22,17 @@ export const Sim = (props) => {
       <hr />
       <p>Data Science</p>
       <Nav vertical>
+      <Menu className=" bg-primary text-white" menuButton={
+        <button className="btn btn-primary" >Calcular de Estadigrafo</button>}>
+          <MenuItem className=" bg-primary" >Media</MenuItem>
+          <MenuItem className=" bg-primary" >Moda</MenuItem>
+          <MenuItem className=" bg-primary" >Desviacion Estandar</MenuItem>
+      </Menu>
         <Button color="primary" onClick={()=>GraficarDesdeArchivo(props.setImagen,props.env,props.camps)}>Graficar Desde Archivo</Button>
         <Button disabled href="#">Metodo1</Button>
         <Button disabled href="#">Metodo2</Button>
         <Button disabled href="#">Metodo3</Button>
-        <Button disabled href="#">Metodo4</Button>
+      
       </Nav>
     </div>
   );
