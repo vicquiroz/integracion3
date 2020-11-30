eecea9c38baed1354d34e5560aa6c534fce90b37
Victor Quiroz <victor.antonio.quiroz.2017@gmail.com>
2020-11-30 18:50:23 -0300

    Lectura de arquetipos
    Se leen los arquetipos y se enlaza de forma correcta con el json de fichas clínicas, todo listo para ser implementado en react
    By: Eduardo Lobos, Fernando Torres y Victor Quiroz

--- PythonDC/Prueba.py
+++ PythonDC/Prueba.py
@@ -12,65 +12,79 @@ def edad(dato):
     fecha_nacimiento = datetime.strptime(fechaStr, "%d-%m-%Y")
     edad = relativedelta(datetime.now(), fecha_nacimiento)
     return edad.years
+      
 
-
-def mostrar(dato,petUno,petDos):
-    guar = [] #arreglo que contendra los datos pedidos
-    for i in range(len(dato)):
-        if (isinstance(dato[i][petUno], list)): #si la primera peticion es un arreglo
-            for j in range(len(dato[i][petUno])): 
-                guar.append(dato[i][petUno][j][petDos]) #guarda los datos de la segunda peticion 
-                
-        else: #sí el anterior no es un arreglo 
-            guar.append(dato[i][petUno]) #se almacenan los datos 
-    return(guar)        
-
-def suma(val):
-    nombres = [] #arreglo que contiene los nombres
+def suma(n,a,r,p,c):
     cont = [] #arreglo que contiene la cantidas de veces repetido el nombre
-    nombres.append(val[0])#se necesita que se inicie con un valor
+    rut = []
+    con = []
+    pos = 0
+    nombresFin = [n[0]+" "+a[0]+"\n"+str(r[0])+" pos:"+str(p[0])+" consulta:"+str(c[0])]
+    rut.append(r[0])
     cont.append(0)#se necesita que se inicie con un valor
-    for i in range(len(val)):
-        resNom = nombres #comparador de nombres
+    con.append(str(c[0]))
+    for i in range(len(n)):
+        resRut = rut #comparador de nombres
         resCont = cont #comparador de cantidad
-        
-        for j in range(len(nombres)):
+        for j in range(len(rut)):
     
-            if nombres[j] == val[i]: #si detecta que almacenado en nombre existe una variable igual a la de los datos
+            if rut[j] == r[i]: #si detecta que almacenado en nombre existe una variable igual a la de los datos
                 cont[j] = cont[j]+1 #le suma 1 a su contador correspondiente
                 resCont = None #se reinicia el comparador
-                resNom = None #se reinicia el comparador
+                resRut = None #se reinicia el comparador
+                if i != 0:
+                    if con[pos] != str(c[i]):
+                        nombresFin[pos] = nombresFin[pos]+","+str(c[i])
 
-        if resNom == nombres and resCont == cont: #si las variables y sus comparadores coinciden
-            nombres.append(val[i]) #se agrega el nombre
+        if resRut == rut and resCont == cont: #si las variables y sus comparadores coinciden
+            con.append(str(c[i]))
+            pos = pos + 1
+            nombresFin.append(n[i]+" "+a[i]+"\n"+str(r[i])+" pos:"+str(p[i])+" consulta:"+str(c[i])) #se agrega el nombre
+            rut.append(r[i])
             cont.append(1) #se agrega un 1 al contador 
 
-    return nombres, cont
+    return nombresFin, cont
 
 with open('prueba.json', encoding='utf-8') as file: #abrir imagen con utf-8 para mayor comprension
-    fichas = json.load(file) #guardamos el contenido del json en data
+    dato = json.load(file) #guardamos el contenido del json en data
     #print(fichas[1])
 
 
 consulta = "Left eye"
 ses = "sesiones_medica"
 arq = "arquetipos"
-cont = 0
 
-guar = []
-pos = []
-for i in range(len(fichas)):
-    for j in range(len(fichas[i][ses])):
-        for k in range(len(fichas[i][ses][j][arq])):
-            for l in range(len(fichas[i][ses][j][arq][k])):
-                #print(fichas[i][ses][j][arq][k][l]["clave"])
-                if consulta == fichas[i][ses][j][arq][k][l]["valor"]:
-                    guar.append(str(fichas[i]["nombre"])+" "+str(fichas[i]["apellidos"]))
-                    pos.append(j)
-                else:
-                    cont = cont+1
-print(guar)
-print(pos)
+def consigue(fichas):
+    nom = []
+    ape = []
+    rut = []
+    pos = []
+    con = []
+    arque = []
+    for i in range(len(fichas)):
+        for j in range(len(fichas[i][ses])):
+            for k in range(len(fichas[i][ses][j][arq])):
+                for l in range(len(fichas[i][ses][j][arq][k])):
+                    #print(fichas[i][ses][j][arq][k][l]["clave"])
+                    if consulta == fichas[i][ses][j][arq][k][l]["valor"]:
+                        nom.append(fichas[i]["nombre"])
+                        ape.append(fichas[i]["apellidos"])
+                        rut.append(fichas[i]["rut"])
+                        pos.append(i)
+                        con.append(j)
+    return nom,ape,rut,pos,con
+
+nom,ape,rut,pos,con = consigue(dato)
+a,b = suma(nom,ape,rut,pos,con)
+for i in range(len(a)):
+    print(a[i])
+    print(b[i])
+#for i in range(len(nom)):
+    #print(nom[i]+" "+ape[i]+"\n"+str(rut[i])+" pos:"+str(pos[i])+" consulta:"+str(con[i]))
+#for i in range(len(nom)):
+    #print(pos[i])
+    #print(con[i])
+#print(pos)
 #print(cont)
 
 
