36444141442da9640242ade46acccaba3f416413
Fernando Torres <ftorres2018@alu.uct.cl>
2020-12-12 22:55:33 -0300

    Avance 2 Codigo comentado y estandarizacion
    Se comento y estandarizo, mostrador y simplifcado.
    By: Jose Moya, Fernando Torres.

--- Proyecto/codx/src/components/General/mostrador.js
+++ Proyecto/codx/src/components/General/mostrador.js
@@ -1,177 +1,154 @@
-import React from 'react'
-import '../comps.css'
-import {Table} from 'reactstrap'
-import * as Zoom from "chartjs-plugin-zoom"
-import {HorizontalBar, Bar,stackedBar} from 'react-chartjs-2'
+import React from "react";
+import "../comps.css";
+import { Table } from "reactstrap";
+import * as Zoom from "chartjs-plugin-zoom";
+import { HorizontalBar } from "react-chartjs-2";
 
-
-
-const getRandomColors = (numbOfBars) =>{
-    const letters = "0123456789ABCDEF".split("");
-    let colors = [];
-    for(let i = 0; i< numbOfBars; i++){
-        let color = "#";
-        for(let k = 0; k < 6 ; k++){
-            color += letters[Math.floor(Math.random() * 16)];
-
-        }
-        colors.push(color);
+const GetRandomColors = (numbOfBars) => { // Constante para generar colores RGB aleatorios ya que la libreria ChartJS no lo hace por defecto
+  const Letters = "0123456789ABCDEF".split("");
+  let colors = [];
+  for (let i = 0; i < numbOfBars; i++) { //Para todo el numero de barras que existan
+    let color = "#"; //Crea un arreglo
+    for (let k = 0; k < 6; k++) {
+      color += Letters[Math.floor(Math.random() * 16)]; //cada valor de color contendra un color RGB
     }
-    return colors;
-}
+    colors.push(color); //inserta el color dentro de colors que contendra cada uno de estos
+  }
+  return colors; //retorna los colores ya generados
+};
 
+export const Mos = (props) => { //constante Mos que recive los datos para generar el grafica mediante props
+  function Grafica(datos) { //funcion grafica que recive los datos
+    if (datos != null) { //si la variable datos no viene vacia
+      var Titulo = []; //Crea los arreglos para contener los titulos y los valores
+      var Valor = [];
+      for (let x in datos) { //for para recorrer todos los datos
+        let Campos = datos[x]; //campos sera cada valor que exista dentro del arreglo de datos
+        Titulo.push(Campos[0]); //Titulo sera igual al primer dato de Campos
+        Valor.push(Campos[1]); //Valor sera igual al segundo valor de Campos
+      }
+      let labs = []; //Se crean arreglos para trabajar con los datos
+      let val = [];
+      let labels = [];
+      let valores = [];
+      labs = Titulo[0];
+      val = Valor[0];
+      for (let x in labs) { //se recorren todos los datos dentro de labs
+        labels.push(labs[x]); //las labels seran cada dato dentro de labs
+        valores.push(val[x]); //los valores de cada uno sera todos los valores dentro de val
+      }
 
-export const Mos = (props) => {
-    function grafica(datos){
-        if(datos!=null){
-            var Titulo=[]
-            var Valor=[]
-            for(let x in datos){
-                let Campos=datos[x]  
-                Titulo.push(Campos[0])
-                Valor.push(Campos[1])
-            } 
-            let labs=[]
-            let val=[]
-            let labels=[]
-            let valores=[]
-            labs = Titulo[0]
-            val = Valor[0]
-            for(let x in labs){
-                labels.push(labs[x])
-                valores.push(val[x])
-            }
-            
+      const Data = { //const data que pasara los datos a ChartJS para generar el grafico
+        labels: labels, //mediante labels entrega los valores para generar las labels
+        datasets: [ //mediante el dataset se le entregan los valores, los que en este caso se encuentran en la variable "valores"
+          {
+            data: valores,
+            backgroundColor: GetRandomColors(labels.length), //se asigna el color para cada barra, que en este caso sera aleatorio usando la funcion generada mas arriba
+            responsive: true, //se define que el grafico sera responsivo con respecto al tamaño de la pagina
+          },
+        ],
+      };
 
-            const data = {
+      const Options = { //constante para definir las opciones del grafico de ChartJS
+        title: { //Se utiliza para definir el titulo que se mostrara en el grafico
+          text: "Grafico de Cantidad",
+          display: true,
+          fontColor: "white",
+        },
 
-                labels:labels,
-            
-                datasets:[{
-                    
-                    //label: "Grafico",
-                    data:valores,
-                    backgroundColor: getRandomColors(labels.length), 
-                    responsive: true,
-                    
+        legend: { //Se utiliza para mostrar la leyenda, en este caso esta desactivada debido a la gran cantidad de datos que se manejan
+          display: false,
+        },
+        tooltips: {//tooltips se utiliza para activar la funcionalidad de al hacer un mouseover, se muestren los datos de cada barra
+          enabled: true,
+        },
+        scales: { //mediante la opcion de scales podemos trabajar con los ejes del grafico
+          yAxes: [ //yAxes para trabajar con el eje Y
+            {
+              ticks: { //le pasamos las opciones para el eje y
+                fontColor: "white",
+                fontSize: 10,
+                stepSize: 0.5,
+                beginAtZero: true,
+              },
+            },
+          ],
+          xAxes: [ //para trabajar con el eje X
+            {
+              ticks: { //le pasamos las opciones del eje X
+                fontColor: "white",
+                fontSize: 10,
+                stepSize: 0.5,
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
 
-                }],      
+        pan: { //mediante la opcion "pan" activamos la opcion de arrastrar el grafico
+          enabled: true,
+          mode: "xy",
+          speed: 100, //velocidad con la que se realizara el arrastre
+        },
 
-             }
-
-             const options = {
-                
-                title: { text: "Grafico de Cantidad", display: true ,fontColor: "white",},
-                
-                        legend: {
-                            display: false
-                         },
-                         tooltips: {
-                            enabled: true
-                         },
-               
-                
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            fontColor: "white",
-                            fontSize: 10,
-                            stepSize: 0.5   ,
-                            beginAtZero: true
-                        }
-                    }],
-                    xAxes: [{
-                        ticks: {
-                            fontColor: "white",
-                            fontSize: 10,
-                            stepSize: 0.5,
-                            beginAtZero: true
-                        }
-                    }]
-                },
-
-                pan: {
-                  enabled: true,
-                  mode: "xy",
-                  speed: 100,
-                  
-                },
-
-                zoom: {
-                  enabled: true,
-                  drag: false,
-                  mode: "xy",
-                  
-                  rangeMin: {
-                    x: 0,
-                    y: 0,
-                  },
-
-                  rangeMax: {
-                    x: 10,
-                    y: 150,
-                  }
-                  
-                }
-              }
-
-            return( 
-            
-                <HorizontalBar  width={200} height={200} options={options} data={data}/>
-            
-            )
-        }
+        zoom: { //con zoom activamos la funcion de poder realizar zoom dentro del grafico
+          enabled: true,
+          drag: false,
+          mode: "xy",
+          rangeMin: {
+            x: 0,
+            y: 0, },
+          rangeMax: {
+            x: 10,
+            y: 150,},
+        },
+      };
+      return (
+        <HorizontalBar width={200} height={200} options={Options} data={Data} /> //retornamos el grafico pasandole los datos, las opciones y definiendo su tamaño
+      );
     }
-    function estadigrafo(estf){
-        if(estf!=null){
-            var Titulo=[]
-            var Valor=[]
-            for(let x in estf){
-                let Campos=estf[x]  
-                Titulo.push(Campos[0])
-                Valor.push(Campos[1])
-            }   
-            let headers=[];
-            for(let x in Titulo){
-                headers.push(<th>{Titulo[x]}</th>)
-            }
-            let partes=[];
-            let body=[];
-            let bodyF=[];
-            for(let x in Valor){
-                partes[x]=Valor[x].toString().split(',');
-            }
-            for(let x in partes[0]){
-                body = []
-                for(let y in partes){
-                    body.push(<th>{partes[y][x]}</th>)
-                }
-                bodyF.push(<tr>{body}</tr>)
-            }
-            return( <Table dark bordered size="xl" responsive>
-                        <thead className="bg-secondary">
-                            <tr>
-                                {headers}
-                            </tr>
-                        </thead>
-                        <tbody className="bg-darker">
-                            {bodyF}
-                        </tbody>
-                    </Table>)
+  }
+
+  function Estadigrafo(estf) { //la funcion de estadigrafo nos retornara la tabla del estadigrafo
+    if (estf != null) { //si los datos recividos no estan vacios
+      var Titulo = []; //crea los arreglos que contendran los valores
+      var Valor = [];
+      for (let x in estf) { //recorre todos los datos
+        let Campos = estf[x]; //para cada dato dentro de estf
+        Titulo.push(Campos[0]); //el titulo sera el primer valor 
+        Valor.push(Campos[1]); //el valor sera el segundo
+      }
+      let headers = []; //se crea arreglo que contendra los titulos
+      for (let x in Titulo) { //recorre todos los titulos
+        headers.push(<th> {Titulo[x]} </th>); //ingresa cada titulo dentro del arreglo titulo
+      }
+      let partes = []; //se crean arreglos para almacenar datos
+      let body = [];
+      let bodyF = [];
+      for (let x in Valor) { //para cada dato en Valor
+        partes[x] = Valor[x].toString().split(","); //se guarda en el arreglo partes el valor convertido a string separado por una coma
+      }
+      for (let x in partes[0]) { //Para cada dato en partes
+        body = []; //Se crea un arreglo
+        for (let y in partes) { //se recorre partes
+          body.push(<th> {partes[y][x]} </th>); //Se crea una fila dentro de body que contiene las partes
         }
+        bodyF.push(<tr> {body} </tr>); // se crea una fila que contiene body
+      }
+      return (
+        <Table dark bordered size="xl" responsive> {/* Retorna la tabla de tamaño xl y que sea responsiva con respecto al tamaño de la pagina */}
+          <thead className="bg-secondary">
+            <tr> {headers} </tr>{" "}
+          </thead>{" "}
+          <tbody className="bg-darker"> {bodyF} </tbody>{" "}
+        </Table>
+      );
     }
-    return(
-        <div>
-                <div>
-                    {grafica(props.datos)}
-                </div>
-            <br/>
-                <div>
-                    {estadigrafo(props.est)}
-                </div>
-        </div>       
-    )
-}
-
-
-
+  }
+  return (
+    <div>
+      <div> {Grafica(props.datos)} </div> <br /> {/*Se retorna la grafica y el estadigrafo hacia la app */}
+      <div> {Estadigrafo(props.est)} </div>{" "}
+    </div>
+  );
+};
--- Proyecto/codx/src/components/General/simplificado.js
+++ Proyecto/codx/src/components/General/simplificado.js
@@ -1,51 +1,137 @@
-import React,{useState} from 'react'
-import { Nav, NavItem, Button, NavbarText,ButtonDropdown,DropdownItem,DropdownToggle,DropdownMenu} from 'reactstrap';
-import {ConseguirArchivo, PostDatos,TablaFrecuenciasDesdeArchivo,GetNombres, GraficarDesdeArchivo, MedianaDesdeArchivo,MediaDesdeArchivo, ModaDesdeArchivo, DesviacionEstandarDesdeArchivo} from '../Comun/consultas';
-import '../comps.css';
-import {Menu,MenuItem } from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css';
+import React, { useState } from "react";
+import {
+  Nav,
+  NavItem,
+  Button,
+  NavbarText,
+  ButtonDropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+} from "reactstrap";
+import {
+  ConseguirArchivo,
+  PostDatos,
+  TablaFrecuenciasDesdeArchivo,
+  GetNombres,
+  GraficarDesdeArchivo,
+  MedianaDesdeArchivo,
+  MediaDesdeArchivo,
+  ModaDesdeArchivo,
+  DesviacionEstandarDesdeArchivo,
+} from "../Comun/consultas";
+import "../comps.css";
+import { Menu, MenuItem } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
 
-export const Sim = (props) => {
+export const Sim = (props) => { //UseState para la dropdown
   const [dropdownOpen, setOpen] = useState(false);
   const toggle = () => setOpen(!dropdownOpen);
 
-  const [Lista,setLista]=useState();
-  const Lst=(ListaRes)=>{
-    setLista(ListaRes)
-  }
+  const [Lista, setLista] = useState(); //UseState para ListaRes
+  const Lst = (ListaRes) => {
+    setLista(ListaRes);
+  };
   return (
     <div id="Simplificado">
-      <Nav>
-        <NavItem>
+      <Nav> {/* Nav viene de reacstrap y define un navegador*/}
+        <NavItem> {/* Se genera cada item dentro de la barra de navegacion*/}
           <NavbarText>Manejo BD</NavbarText>
-            <div className="btn-group btn-sm">
+          <div className="btn-group btn-sm">
             <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
-              <Button id="caret" color="primary" onClick={()=>GetNombres(props.res,Lst)}>Cargar lista</Button>
+              <Button 
+                id="caret"
+                color="primary"
+                onClick={() => GetNombres(props.res, Lst)} 
+              >{/* Boton para cargar las listas */}
+                Cargar lista
+              </Button>
               <DropdownToggle split color="primary" />
-              <DropdownMenu>
+              <DropdownMenu>{/* Dropdown que contendra las listas cargadas previamente*/}
                 <DropdownItem></DropdownItem>
-                {Lista}
+                {Lista} {/* Items del dropdown */}
                 <DropdownItem></DropdownItem>
               </DropdownMenu>
             </ButtonDropdown>
-            <Button color="info" onClick={()=>PostDatos(props.env)}>Actualizar</Button>
-            </div>
-            
+            <Button color="info" onClick={() => PostDatos(props.env)}> {/* Boton que subira el archivo a la base de datos*/}
+              Actualizar
+            </Button>
+          </div>
         </NavItem>
-        <br/>
-      <NavItem>
-        <NavbarText>    Data Science  </NavbarText>
-          <Menu className=" bg-info text-white border border-primary" menuButton={
-            <button className="btn btn-primary btn-md" >Calcular de Estadigrafo</button>}>
-              <MenuItem className=" bg-info" onClick={()=>MedianaDesdeArchivo(props.setEstadigrafo,props.env,props.camps)} >Mediana</MenuItem>
-              <MenuItem className=" bg-info" onClick={()=>MediaDesdeArchivo(props.setEstadigrafo,props.env,props.camps)} >Media Aritmetica</MenuItem>
-              <MenuItem className=" bg-info" onClick={()=>ModaDesdeArchivo(props.setEstadigrafo,props.env,props.camps)}>Moda</MenuItem>
-              <MenuItem className=" bg-info" onClick={()=>DesviacionEstandarDesdeArchivo(props.setEstadigrafo,props.env,props.camps)}>Desviacion Estandar</MenuItem>
+        <br />
+        <NavItem>
+          <NavbarText>Data Science</NavbarText> {/* Seccion de Data Science dentro del navegador*/}
+          <Menu
+            className=" bg-info text-white border border-primary"
+            menuButton={
+              <button className="btn btn-primary btn-md"> {/* Boton para generar el estadigrafo*/}
+                Calcular de Estadigrafo
+              </button>
+            }
+          >
+            <MenuItem 
+              className=" bg-info"
+              onClick={() =>
+                MedianaDesdeArchivo(
+                  props.setEstadigrafo,
+                  props.env,
+                  props.camps
+                )
+              }
+            > {/* Item dentro del menu para generar la mediana*/}
+              Mediana
+            </MenuItem>
+            <MenuItem
+              className=" bg-info"
+              onClick={() =>
+                MediaDesdeArchivo(props.setEstadigrafo, props.env, props.camps)
+              }
+            > {/* Item dentro del menu para generar la media*/}
+              Media Aritmetica
+            </MenuItem>
+            <MenuItem
+              className=" bg-info"
+              onClick={() =>
+                ModaDesdeArchivo(props.setEstadigrafo, props.env, props.camps)
+              }
+            > {/* Item dentro del menu para generar la Moda*/}
+              Moda
+            </MenuItem>
+            <MenuItem
+              className=" bg-info"
+              onClick={() =>
+                DesviacionEstandarDesdeArchivo(
+                  props.setEstadigrafo,
+                  props.env,
+                  props.camps
+                )
+              }
+            > {/* Item dentro del menu para generar la Desviacion Estandar*/}
+              Desviacion Estandar
+            </MenuItem>
           </Menu>
-            <Button color="primary" onClick={()=>GraficarDesdeArchivo(props.setGrafico,props.env,props.camps)}>Graficar Desde Archivo</Button>
-            <Button color="primary" onClick={()=>TablaFrecuenciasDesdeArchivo(props.setEstadigrafo,props.env,props.camps)}>Tabla de Frecuencias</Button>
+          <Button
+            color="primary"
+            onClick={() =>
+              GraficarDesdeArchivo(props.setGrafico, props.env, props.camps)
+            }
+          > {/* Boton para generar grafico con los datos seleccionados*/}
+            Graficar Desde Archivo
+          </Button>
+          <Button
+            color="primary"
+            onClick={() =>
+              TablaFrecuenciasDesdeArchivo(
+                props.setEstadigrafo,
+                props.env,
+                props.camps
+              )
+            }
+          > {/* Boton para generar la tabla de frecuencias*/}
+            Tabla de Frecuencias
+          </Button>
         </NavItem>
       </Nav>
     </div>
   );
-}
+};
